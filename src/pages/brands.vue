094f775f76a948ba25d79af32ba88556fb1b3029
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2018-03-11 16:12:45 +0100

    [ADD] brands page

--- src/pages/brands.vue
+++ src/pages/brands.vue
@@ -7,4 +7,5 @@
     a reader implementing at least that specification to display the file.
     Note that $20 here means the character 'white space'.
   </p>
+  <table is="mp4ra-table" v-bind:columns="['code', 'description', 'specification']" v-bind:data="mp4ra.brands.db"></table>
 </main>
